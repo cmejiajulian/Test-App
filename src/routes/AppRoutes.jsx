7f04c813079a868aa51cf55b840724842f6b869b
Julian <cmejiajulian@gmail.com>
2025-04-24 10:35:24 -0500

    Parte 4: Creacion de formulario de pago y entrega con Redux y validación básica

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import CatalogView from '../features/catalog/CatalogView'
+import CheckoutForm from '../features/checkout/CheckoutForm'
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<CatalogView />} />
+      <Route path="/checkout" element={<CheckoutForm />} />
     </Routes>
   )
 }
