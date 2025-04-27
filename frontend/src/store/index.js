cf51b43bd7546a44209e71193510bb4c4cd9b98f
Julian <cmejiajulian@gmail.com>
2025-04-26 22:00:16 -0500

    Corrige fronted y checkout-app-backup: elimina subrepos y agrega como carpetas normales

---
+++ frontend/src/store/index.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import catalogReducer from "../features/catalog/catalogSlice"
+
+export const store = configureStore({
+    reducer:{
+        catalog:catalogReducer
+    }
+})
