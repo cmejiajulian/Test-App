3be3d9d507c6bbdeb647f86d7f4971a41c35a75b
Julian <cmejiajulian@gmail.com>
2025-04-27 11:34:28 -0500

    remove checkout-app

--- frontend/src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import catalogReducer from "../features/catalog/catalogSlice"
-
-export const store = configureStore({
-    reducer:{
-        catalog:catalogReducer
-    }
-})
